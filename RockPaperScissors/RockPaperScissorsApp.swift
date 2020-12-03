1af0fe1dfbbbf11ce0009892b47985d3e9f6b10e
Pavel Sakhanko <iospaveldev@gmail.com>
2020-12-03 21:32:02 +0300

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Pavel Sakhanko on 3.12.20.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
